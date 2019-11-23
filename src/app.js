b76bd641dc542d4154922035810fbda5c2613eda
Rudy Marchandise <r.marchandise@sfr.fr>
2019-11-23 22:13:57 +0100

    body parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
-const express = require('express')();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const multer  = require('multer')();
 const config = require('./nconf');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(multer.array()); 
 
 function response(req) {
     if (req.originalUrl != "/ping" || !config.get('logs:ignore:ping')) {
@@ -29,5 +39,5 @@ function response(req) {
     }
 };
 
-express.all('*', (req, res) => res.json(response(req)));
-express.listen(config.get('port'), () => console.log(`Listening on port ${config.get('port')}.`));
+app.all('*', (req, res) => res.json(response(req)));
+app.listen(config.get('port'), () => console.log(`Listening on port ${config.get('port')}.`));
