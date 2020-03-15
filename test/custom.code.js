e88797199cd6163b0825066f4bfae9368b92aef6
Marchandise Rudy <rudy.marchandise@protonmail.com>
2020-03-15 18:14:48 +0100

    Randomize status code (#41)
    * Randomize status code response

--- test/custom.code.js
+++ test/custom.code.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const assert = require('assert');
 
 describe('Custom HTTP Code', function () {
   var server;
@@ -89,5 +90,101 @@ describe('Custom HTTP Code', function () {
     request(server)
       .get('/?echo_code=500')
       .expect(500, done);
+  });
+  for (let a=200;a<203;a++){
+    for (let b=400;b<403;b++){
+      it(`Custom rand status ${a} or ${b} with header`, (done) => {
+        request(server)
+        .get('/')
+        .set('X-ECHO-CODE', `${a}-${b}`)
+        .end((err, res) => {
+          assert.equal(true, res.status == a || res.status == b);
+          assert.equal(res.header['x-echo-random-status'], `${a}, ${b}`);
+          done();
+        })
+      });
+    }
+  }
+  for (let a=200;a<202;a++){
+    for (let b=300;b<302;b++){
+      for (let c=400;c<402;c++){
+        it(`Custom rand status ${a}, ${b}, ${c} with header`, (done) => {
+          request(server)
+          .get('/')
+          .set('X-ECHO-CODE', `${a}-${b}-${c}`)
+          .end((err, res) => {
+            assert.equal(true, res.status == a || res.status == b || res.status == c);
+            assert.equal(res.header['x-echo-random-status'], `${a}, ${b}, ${c}`);
+            done();
+          })
+        });
+      }
+    }
+  }
+  for (let a=200;a<203;a++){
+    for (let b=400;b<403;b++){
+      it(`Custom rand status ${a} or ${b} with query`, (done) => {
+        request(server)
+        .get('/?echo_code='+`${a}-${b}`)
+        .end((err, res) => {
+          assert.equal(true, res.status == a || res.status == b);
+          assert.equal(res.header['x-echo-random-status'], `${a}, ${b}`);
+          done();
+        })
+      });
+    }
+  }
+  for (let a=200;a<202;a++){
+    for (let b=300;b<302;b++){
+      for (let c=400;c<402;c++){
+        it(`Custom rand status ${a}, ${b}, ${c} with query`, (done) => {
+          request(server)
+          .get('/?echo_code='+`${a}-${b}-${c}`)
+          .end((err, res) => {
+            assert.equal(true, res.status == a || res.status == b || res.status == c);
+            assert.equal(res.header['x-echo-random-status'], `${a}, ${b}, ${c}`);
+            done();
+          })
+        });
+      }
+    }
+  }
+  it("Custom with null separator", (done) => {
+    request(server)
+      .get('/')
+      .set('X-ECHO-CODE', "200.401")
+      .end((err, res) => {
+        assert.equal(200, res.status);
+        assert.equal(res.header['x-echo-random-status'], undefined);
+        done();
+      })
+  });
+  it("Custom with null args in Query", (done) => {
+    request(server)
+      .get('/?echo_code=200/404')
+      .end((err, res) => {
+        assert.equal(200, res.status);
+        assert.equal(res.header['x-echo-random-status'], undefined);
+        done();
+      })
+  });
+  it("Custom with null separator but correct value", (done) => {
+    request(server)
+      .get('/')
+      .set('X-ECHO-CODE', "200.401-401")
+      .end((err, res) => {
+        assert.equal(401, res.status);
+        assert.equal(res.header['x-echo-random-status'], '401');
+        done();
+      })
+  });
+  it("Custom with null separator but correct value", (done) => {
+    request(server)
+      .get('/?echo_code=200.401-404')
+      .end((err, res) => {
+        assert.equal(res.status, 404);
+        assert.equal(res.header['x-echo-random-status'], '404');
+        done();
+      });
   });
 });
